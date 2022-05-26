ebd43750d2adbabac3a9b9d9b911d3cabc8523bb
eaCe <webmaster@slaps.de>
2022-05-26 20:48:09 +0200

    feat: add additional secret

--- install.php
+++ install.php
@@ -13,3 +13,8 @@ rex_sql_table::get(rex::getTable($this->getName()))
     ->ensureColumn(new rex_sql_column('hash', 'varchar(255)', false))
     ->ensureColumn(new rex_sql_column('expiration', 'datetime', false))
     ->ensure();
+
+if (!$this->getProperty('secret')) {
+    $yaml = array_merge(rex_file::getConfig($this->getPath() . rex_package::FILE_PACKAGE), array('secret' => bin2hex(random_bytes(40))));
+    rex_file::putConfig($this->getPath() . rex_package::FILE_PACKAGE, $yaml);
+}
