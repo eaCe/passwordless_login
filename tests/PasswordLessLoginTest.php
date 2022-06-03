4b6b29aff91649a36bf0eb49c14e6f9c3adf1f94
eaCe <webmaster@slaps.de>
2022-06-03 11:43:40 +0200

    chore: add some basic tests

--- tests/PasswordLessLoginTest.php
+++ tests/PasswordLessLoginTest.php
@@ -16,4 +16,50 @@ final class PasswordLessLoginTest extends TestCase
         $this->assertSame('test-route', PasswordLessLogin::$route);
         $this->assertSame('test-login-route', PasswordLessLogin::$loginRoute);
     }
+
+    /**
+     * @return void
+     * @throws rex_exception
+     * @throws rex_http_exception
+     * @throws rex_sql_exception
+     */
+    public function testWrongLoginRequestMethod(): void {
+        $this->expectException(rex_exception::class);
+        PasswordLessLogin::$loginRoute = 'pll';
+        $_SERVER['REQUEST_URI'] = '/pll/';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+
+        PasswordLessLogin::handleLogin();
+    }
+
+    /**
+     * @return void
+     * @throws rex_exception
+     * @throws rex_http_exception
+     * @throws rex_sql_exception
+     */
+    public function testMissingLoginHash(): void {
+        $this->expectException(rex_exception::class);
+        PasswordLessLogin::$loginRoute = 'pll';
+        $_SERVER['REQUEST_URI'] = '/pll/';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+
+        PasswordLessLogin::handleLogin();
+    }
+
+    /**
+     * @return void
+     * @throws rex_exception
+     * @throws rex_http_exception
+     * @throws rex_sql_exception
+     */
+    public function testMissingLoginSecret(): void {
+        $this->expectException(rex_exception::class);
+        PasswordLessLogin::$loginRoute = 'pll';
+        $_SERVER['REQUEST_URI'] = '/pll/';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_GET['hash'] = uniqid('', false);
+
+        PasswordLessLogin::handleLogin();
+    }
 }
