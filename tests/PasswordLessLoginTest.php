4bf3b5f85acd7eb694148fd3ea36c2c231619b0f
eaCe <webmaster@slaps.de>
2022-09-04 09:25:09 +0200

    chore: add some tests

--- tests/PasswordLessLoginTest.php
+++ tests/PasswordLessLoginTest.php
@@ -15,7 +15,7 @@ final class PasswordLessLoginTest extends TestCase
         PasswordLessLogin::$loginRoute = \rex_extension::registerPoint(new \rex_extension_point('PLL_LOGIN_ROUTE', 'test-login-route'));
 
         \rex_config::set('testns', 'testeky', 'test');
-        
+
         $this::assertSame('test-route', PasswordLessLogin::$route);
         $this::assertSame('test-login-route', PasswordLessLogin::$loginRoute);
     }
@@ -29,6 +29,8 @@ final class PasswordLessLoginTest extends TestCase
     public function testWrongLoginRequestMethod(): void
     {
         $this->expectException(\rex_exception::class);
+        $this->expectExceptionMessage('Request-Method not allowed');
+
         PasswordLessLogin::$loginRoute = 'pll';
         $_SERVER['REQUEST_URI'] = '/pll/';
         $_SERVER['REQUEST_METHOD'] = 'POST';
@@ -45,6 +47,8 @@ final class PasswordLessLoginTest extends TestCase
     public function testMissingLoginHash(): void
     {
         $this->expectException(\rex_exception::class);
+        $this->expectExceptionMessage('Hash missing');
+
         PasswordLessLogin::$loginRoute = 'pll';
         $_SERVER['REQUEST_URI'] = '/pll/';
         $_SERVER['REQUEST_METHOD'] = 'GET';
@@ -61,11 +65,49 @@ final class PasswordLessLoginTest extends TestCase
     public function testMissingLoginSecret(): void
     {
         $this->expectException(\rex_exception::class);
+        $this->expectExceptionMessage('Secret missing');
+
         PasswordLessLogin::$loginRoute = 'pll';
         $_SERVER['REQUEST_URI'] = '/pll/';
         $_SERVER['REQUEST_METHOD'] = 'GET';
         $_GET['hash'] = uniqid('', false);
 
         PasswordLessLogin::handleLogin();
+    }
+
+    /**
+     * @return void
+     * @throws rex_exception
+     * @throws rex_http_exception
+     * @throws rex_sql_exception
+     * @throws \PHPMailer\PHPMailer\Exception
+     */
+    public function testWrongRequestMethod(): void
+    {
+        $this->expectException(\rex_http_exception::class);
+        $this->expectExceptionMessage('Request-Method not allowed');
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = 'test-route';
+
+        PasswordLessLogin::handleSubmit();
+    }
+
+    /**
+     * @return void
+     * @throws rex_exception
+     * @throws rex_http_exception
+     * @throws rex_sql_exception
+     * @throws \PHPMailer\PHPMailer\Exception
+     */
+    public function testMissingEmail(): void
+    {
+        $this->expectException(\rex_http_exception::class);
+        $this->expectExceptionMessage('E-Mail missing');
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_SERVER['REQUEST_URI'] = 'test-route';
+
+        PasswordLessLogin::handleSubmit();
     }
 }
